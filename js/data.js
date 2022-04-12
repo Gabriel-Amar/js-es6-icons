9ad3476aaa23dd5eb31139ab418f940d1565b6eb
Gabriel-Amar <amariutei.gabriel1@gmail.com>
2022-04-12 10:55:05 +0200

    index, css e js: aggiunta colori casuali

--- js/data.js
+++ js/data.js
@@ -113,16 +113,28 @@ const icons =[
 	}
 ];
 
+function getRandomColor() {
+	var letters = '0123456789ABCDEF';
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+	  color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+icons.forEach((icona)=>{
+	icona.color = getRandomColor();
+})
+
 let container = document.querySelector(".row")
 stampa(icons)
 
-function stampa(array){
+function stampa(icons){
 	let div = "";
-	array.forEach((element) =>{
+	icons.forEach((element) =>{
 			div += 
 			`
-			<div class="col-2">
-				<div class="square"><i style= "color:${element.color}" class="${element.prefix}solid fa-${element.name}"></i><span>${element.name}</span></div>
+			<div class="col-12 col-sm-12 col-md-6 col-lg-3">
+				<div class="square"><i style= "color:${element.color}" class="${element.prefix}solid fa-${element.name}"></i><span>${element.name.toUpperCase()}</span></div>
 			</div>
 			`
 	})
@@ -133,15 +145,17 @@ function stampa(array){
 let select = document.querySelector(".form-select");
 
 select.addEventListener("change", function(){
+	//Questo this si riferisce al select qua sopra! Potevi usarlo al posto di richiamare select.value
+	console.log(this)
 
 	let tipo = select.value;
-	if(tipo === "1"){
+	if(tipo === "animal"){
 		container.innerHTML = "";
 		stampa(icons.filter((icone) => icone.type === "animal"));
-	} else if(tipo === "2"){
+	} else if(tipo === "vegetable"){
 		container.innerHTML = "";
 		stampa(icons.filter((icone)=> icone.type === "vegetable"));
-	}else if(tipo === "3"){
+	}else if(tipo === "user"){
 		container.innerHTML = "";
 		stampa(icons.filter((icone)=> icone.type === "user")); 
 	}else if(tipo === "0"){
